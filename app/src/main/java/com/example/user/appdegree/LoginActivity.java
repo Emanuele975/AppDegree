771af459d3236e9bcbe8478b58532be98668f266
Emanuele Flammini <emanueleflammini97@gmail.com>
2020-02-13 10:05:01 +0100

    bottone camera

--- app/src/main/java/com/example/user/appdegree/LoginActivity.java
+++ app/src/main/java/com/example/user/appdegree/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.user.appdegree;
 
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnInvio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Bundle richiesta = getIntent().getExtras();
+        btnInvio=findViewById(R.id.button3);
+
+        btnInvio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openCamera = new Intent(LoginActivity.this, PhotoActivity.class);
+                startActivity(openCamera);
+                //openLogin.putExtra("richiesta", true);
+            }
+        });
     }
 }
